2d9416475ee1bf8a34f2aa15a2bc1c0a2a7e6a2b
Andrew <cibisova267@gmail.com>
2023-03-31 16:28:48 +0500

    Начал адаптацию страницы

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        'main': 'repeat(auto-fit, minmax(180px, 1fr))',
+      }
+    },
+    screens: {
+      'sm': '500px',
+      'md': '900px',
+      'lg': '1100px',
+      'xl': '1400px',
+      '2xl': '1600px',
+      '3xl': '1800px',
+      '4xl': '2000px',
+      '5xl': '2200px',
+    },
   },
   plugins: [
     require('@tailwindcss/line-clamp')
